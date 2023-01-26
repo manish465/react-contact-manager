a3400e0acc3feb384d3457a4ab3265086811e8dc
manish <46858945+manish465@users.noreply.github.com>
2023-01-26 15:30:35 +0530

    added login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return <div>Login</div>;
+    return (
+        <>
+            <section className="user login">
+                <h1>User Login</h1>
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <div className="button-group">
+                    <Link to="/register">
+                        <button className="secondary hover-anmiation">
+                            Register
+                        </button>
+                    </Link>
+                    <button className="primary hover-anmiation">Login</button>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Login;
