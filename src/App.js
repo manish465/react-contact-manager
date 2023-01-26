5e0ccec655d5f347aa2cb1754464b01b00157fe2
manish <46858945+manish465@users.noreply.github.com>
2023-01-26 13:41:36 +0530

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
-    return <h1>Hello World</h1>;
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    );
 };
 
 export default App;
