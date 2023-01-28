263ca6278989604fc3d65e7b20b92542d961e848
manish <ms5978307@gmail.com>
2023-01-28 14:03:24 +0530

    bug fix

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { tokenContext } from "../context/TokenContext";
 
 const Contact = () => {
     const [contact, setContact] = useState(null);
     const { currentUser } = useContext(tokenContext);
     const { contactId } = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios
@@ -17,65 +18,78 @@ const Contact = () => {
                 }
             )
             .then(({ data }) => {
-                if (data.code === 200) setContact(data.contact);
-                else console.log(data);
+                if (data.code === 200) {
+                    setContact(data.contact);
+                } else {
+                    console.log(data);
+                }
             })
-            .catch(({ message }) => console.log(message));
+            .catch(({ message }) => {
+                console.log(message);
+            });
     }, [contactId, currentUser.id, currentUser.token]);
 
     return (
         <section id="profile">
             <h1>PROFILE</h1>
-            <h3>
-                <span>name : </span>
-                <span>
-                    {contact
-                        ? contact.firstName + " " + contact.lastName
-                        : "feching....."}
-                </span>
-            </h3>
-            <h3>
-                <span>email : </span>
-                <span>{contact ? contact.email : "feching....."}</span>
-            </h3>
-            <h3>
-                <span>company name : </span>
-                <span>
-                    {contact ? contact.work.companyName : "feching....."}
-                </span>
-            </h3>
-            <h3>
-                <span>job designation : </span>
-                <span>
-                    {contact ? contact.work.jobDesignation : "feching....."}
-                </span>
-            </h3>
-            {contact.phoneNumbers.map((phoneNumber, key) => (
-                <div key={key}>
+            {contact ? (
+                <>
+                    <h3>
+                        <span>name : </span>
+                        <span>
+                            {contact.firstName + " " + contact.lastName}
+                        </span>
+                    </h3>
+                    <h3>
+                        <span>email : </span>
+                        <span>{contact.email}</span>
+                    </h3>
+                    <h3>
+                        <span>company name : </span>
+                        <span>{contact.work.companyName}</span>
+                    </h3>
+                    <h3>
+                        <span>job designation : </span>
+                        <span>{contact.work.jobDesignation}</span>
+                    </h3>
+                    {contact.phoneNumbers.map((phoneNumber, key) => (
+                        <div key={key}>
+                            <h3>
+                                <span>country code : </span>
+                                <span>
+                                    {contact
+                                        ? phoneNumber.countryCode
+                                        : "feching....."}
+                                </span>
+                            </h3>
+                            <h3>
+                                <span>number : </span>
+                                <span>
+                                    {contact
+                                        ? phoneNumber.number
+                                        : "feching....."}
+                                </span>
+                            </h3>
+                            <h3>
+                                <span>type : </span>
+                                <span>
+                                    {contact
+                                        ? phoneNumber.type
+                                        : "feching....."}
+                                </span>
+                            </h3>
+                        </div>
+                    ))}
                     <h3>
                         <span>country code : </span>
                         <span>
-                            {contact ? phoneNumber.countryCode : "feching....."}
+                            {contact ? contact.description : "feching....."}
                         </span>
                     </h3>
-                    <h3>
-                        <span>number : </span>
-                        <span>
-                            {contact ? phoneNumber.number : "feching....."}
-                        </span>
-                    </h3>
-                    <h3>
-                        <span>type : </span>
-                        <span>
-                            {contact ? phoneNumber.type : "feching....."}
-                        </span>
-                    </h3>
-                </div>
-            ))}
-            <h3>
-                <span>country code : </span>
-                <span>{contact ? contact.description : "feching....."}</span>
-            </h3>
+                </>
+            ) : (
+                ""
+            )}
             <div className="button-group">
                 <button className="secondary hover-anmiation">
                     Update Contact
